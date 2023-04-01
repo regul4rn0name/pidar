0d4416e63f0ada329f21ecee82751b7c7e20e1fb
regul4rn0name <komlyk07@gmail.com>
2023-04-01 10:47:44 +0200

    ss
    ss

--- Assets/Scripts/Playermovementmario.cs
+++ Assets/Scripts/Playermovementmario.cs
@@ -1,64 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Playermovementmario : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
     public float speed;
     public float jump;
     private bool jumping = false;
     private bool left = false;
     private bool right = false;
-    private bool air = false;
-    // Update is called once per frame
+    public bool air = false;
+    public float friction;
+    public float horizontal;
+   
+    
+    
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        
+        
         if (Input.GetKey("a"))
         {
             left = true;
+            animator.SetBool("runmarioL", true);
+           
+            
+          
+            
+
         }
         else
         {
             left = false;
+            animator.SetBool("runmarioL", false);
         }
         if (Input.GetKey("d"))
         {
             right = true;
+            animator.SetBool("runmarioR", true);
         }
         else
         {
             right = false;
-
+            animator.SetBool("runmarioR", false);
         }
         if (Input.GetKey("w"))
         {
             jumping = true;
-
+            animator.SetBool("jumpmario", true);
         }
         else
         {
             jumping = false;
+            animator.SetBool("jumpmario", false);
         }
+       
 
 
     }
     private void FixedUpdate()
     {
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        
         if (left)
         {
-            rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            
         }
         if (right)
         {
-            rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
+
+        // Apply friction if the object is moving and not jumping
+        if (rb.velocity.magnitude > 0 && !air|| Input.GetKey("a") || Input.GetKey("d"))
+        {
+            rb.AddForce(-rb.velocity.normalized * friction, ForceMode2D.Force);
+        }
+
         if (jumping && !air)
         {
             rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
             air = true;
+            
         }
+
+
+
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -67,16 +101,37 @@ public class Playermovementmario : MonoBehaviour
         {
 
             air = false;
+            
         }
         if (other.gameObject.CompareTag("Brick"))
         {
-
             air = false;
+
         }
-        if (other.gameObject.CompareTag("enemy"))
+        if (other.gameObject.CompareTag("enemy") && !air)
         {
 
             SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+        
+        if (other.gameObject.CompareTag("mushroom"))
+        {
+
+            Transform objectTransform = GetComponent<Transform>();
+
+            // Double the object's scale
+            objectTransform.localScale *= 1.2f;
+            
+        }
+        if (other.gameObject.CompareTag("Respawn"))
+        {
+
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+        }
+        if (other.gameObject.CompareTag("Respawn"))
+        {
+
+            SceneManager.LoadScene("galaga", LoadSceneMode.Single);
         }
 
     }
--- Assets/Sprite/Solonenko/bl.cs
+++ Assets/Sprite/Solonenko/bl.cs
@@ -1,26 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class bl : MonoBehaviour
- {   
+{
     public float speed = 5f;
-   private int enemulin = 4;
+    
     void Update()
-    {  
+    {
         // перемещаем объект вперед с постоянной скоростью
         transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
-   private void OnTriggerEnter2D(Collider2D collision)
-{
-    // проверяем, касается ли объект объекта с тегом "dedwall"
-    if (collision.gameObject.CompareTag("dedwall"))
-    {
-        enemulin--;
-        // уничтожаем объект
-        Destroy(gameObject);
-    }
-    
-}
 }
